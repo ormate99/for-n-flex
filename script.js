8ce514385d6e3caa3a7707f1a41aa342e582332d
ormate99 <ormate99@gmail.com>
2021-11-18 13:54:50 +0100

    first fix attempted

--- script.js
+++ script.js
@@ -27,24 +27,43 @@ const generateArray = function(amount){
 
 function loadEvent() {
     console.log("the page has loaded");
-
+    
     /* console.log(generateArray("kiskutya"));
     console.log(generateArray("1 kiskutya"));
     console.log(generateArray(100));
     console.log(generateArray("100"));
     console.log(generateArray([100]));
     */
-
-    const root = document.getElementById("root");
-    const list = generateArray(6);
-    console.log(list);
-
-    if (list[0]  !== "error") {
-        for (const item of list) {
-            root.insertAdjacentHTML("beforeend", `<div>${item}</div>`)
+   
+   const root = document.getElementById("root");
+   const list = generateArray(100);
+   const num = list.length;
+   var digits = (num, count = 0) => {
+       if (num) {
+           return digits(Math.floor(num / 10), ++count);
+        };
+        return count;
+    }
+    var nulla = '0'
+    
+   console.log(digits(num));
+   console.log(list);
+   
+   
+   if (list[0]  !== "error") {
+       for (const item of list) {
+           root.insertAdjacentHTML("beforeend", `<div>${nulla.repeat(digits(num))+item}</div>`);
+           // root.insertAdjacentHTML("beforeend", ('0' + item).slice(-digit)); mögé rakja nem bele
         }
     }
+    
 };
+
+
+
+
+
+
 
 window.addEventListener("load", loadEvent);
 
